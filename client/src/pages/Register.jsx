51952ceb1ce3885d9fc712cbf8b53959ddcab29f
JordanGallant <39-12602@sae.edu.za>
2023-09-01 21:35:52 +0200

    adding-proxy

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,29 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 export const Register = () => {
   const [inputs, setInputs] = useState({
-    username:"",
-    email:"",
-    password:"",
-  
-  })
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-  const handleChange= e =>{
-    setInputs(prev=>({...prev,[e.target.name]: e.target.value}))
+  const handleSubmit = async e =>{
+    e.preventDefault()
+    try{
+      const res = await axios.post("auth/register", inputs)
+    } catch (err) {
+      console.log(err)
+    }
+    
   }
+
+  
   return (
-    <div className='auth'>
-        <h1>Register</h1>
-        <form>
-            <input required type='text' placeholder='username' name = "username" onChange={handleChange}/>
-            <input required type= 'email' placeholder = 'email' name = "email" onChange={handleChange}/>
-            <input required type='password' placeholder='password' name = "password" onChange={handleChange}/>
-            
-            <button>Login</button>
-            <p>This is an error</p>
-            <span>Do you have an account? <Link to= '/login'> Login</Link> </span>
-        </form>
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+
+        <button onClick={handleSubmit}>Register</button>
+        <p>This is an error</p>
+        <span>
+          Do you have an account? <Link to="/login"> Login</Link>{" "}
+        </span>
+      </form>
     </div>
-  )
-}
+  );
+};
