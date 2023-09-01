6af756d5a7b9fbf6773246cd8a17dc122f598fe8
JordanGallant <39-12602@sae.edu.za>
2023-09-01 06:57:33 +0200

    init-controllers

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
+import postRoutes from "./routes/posts.js"
 import express from "express";
 
 const app = express()
 
+
 app.use(express.json())
+app.use("/api/posts", postRoutes)
+
 
 app.get("/test", (req,res)=>{
     res.json("it works")
